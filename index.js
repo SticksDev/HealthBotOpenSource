412bcff4e2b665f2cf7c3348d88a62668192d795
thatcooldevguy <tsommers150@gmail.com>
2021-03-17 21:00:10 -0400

    add key system

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
 const Discord = require("discord.js");
 const Enmap = require("enmap");
 const fs = require("fs");
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest
 
 const client = new Discord.Client();
 const config = require("./config.json");
 
+// Please do not edit the below, this is the key checker. Doing so will result in a key removal.
+if(config.selfhost === "false") {
+  throw new Error("This is the open source version of the bot, not the main one. Please set selfhost in config to true.")
+}
+if(config.key === "") {
+  throw new Error("No self host key was set in config. Killing.")
+}
+function existsFile(url) {
+  var http = new XMLHttpRequest();
+  http.open('HEAD', url, false);
+  http.send();
+  return http.status!=404;
+}
 
+if(existsFile(`http://sticks.cf/healthbot/${config.key}`) === true) {
+  console.log("Key OK")
+} else {
+  throw new Error("Key not OK.")
+}
+// Start Editable code
 
 client.config = config;
 
